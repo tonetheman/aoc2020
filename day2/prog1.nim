4079ca997561d2d7c13e3efe83d8c8604bb24e9b
Tony Colston <tonetheman@gmail.com>
2020-12-02 00:45:14 -0600

    part2

--- day2/prog1.nim
+++ day2/prog1.nim
@@ -4,7 +4,6 @@ import strutils as su
 
 proc validate(lo,hi:int,val,passwd:string):bool =
     let count = su.count(passwd,val)
-    # echo("count ", count)
     if count >= lo and count <= hi:
         return true
     return false
@@ -25,7 +24,39 @@ proc prob1(filename : string) =
             valid_count += 1
     echo("valid count is ",valid_count)
 
+proc validate2(lo,hi:int,val,passwd:string):bool =
+    var count : int = 0
+
+    if passwd[lo-1]==val[0]:
+        count+=1
+
+    if passwd[hi-1]==val[0]:
+        count+=1
+
+    if count==1:
+        return true
+
+    return false
+
+proc prob2(filename: string) =
+    var inf : File
+    inf = open(filename)
+    defer: inf.close()
+    var line : string
+    var lo, hi : int
+    var val : string
+    var passwd : string
+    var valid_count : int = 0
+    while inf.readline(line):
+        discard scanf(line,"$i-$i $w: $w",lo,hi,val,passwd)
+        echo("low and hi: ",lo," ",hi," ",val," ",passwd)
+        if validate2(lo,hi,val,passwd):
+            valid_count += 1
+    echo("valid count is ",valid_count)
+
 
 
 # prob1("testinput.txt")
-prob1("input.txt")
+# prob1("input.txt")
+# prob2("testinput.txt")
+prob2("input.txt")
