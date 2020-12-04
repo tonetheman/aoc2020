748665e359edeaa362bf786da14437a8d18cd769
tonetheman <tonetheman@gmail.com>
2020-12-04 16:40:39 -0600

    started on part 2 not done

--- day4/prog.nim
+++ day4/prog.nim
@@ -1,11 +1,16 @@
 
 import strutils
+import re
+
 var person : int = 1
 
 type PD = tuple
     byr : bool
+    birth_year : int
     iyr : bool
+    issue_year : int
     eyr : bool
+    expiration_year : int
     hgt : bool
     hcl : bool
     ecl : bool
@@ -24,13 +29,24 @@ proc clear_PD(p : var PD) =
 
 proc set_PD(thingdata: seq[string], p : var PD) =
     # echo("set PD called ",p)
-    for thing in thingdata:
+    for index, thing in thingdata:
         #  echo("\tcurrent thing : ", thing)
         if thing=="byr":
+            # if match(thingdata[index+1],re"^(\d\d\d\d)$"):
+            #    echo("BYR MATCH")
+            p.birth_year = parseInt(thingdata[index+1])
+            if p.birth_year >= 1920 and p.birth_year <= 2002:
+                discard
             p.byr = true
         elif thing=="iyr":
+            p.issue_year = parseInt(thingdata[index+1])
+            if p.issue_year >= 2010 and p.issue_year <= 2020:
+                discard
             p.iyr = true
         elif thing=="eyr":
+            p.expiration_year = parseInt(thingdata[index+1])
+            if p.expiration_year >= 2020 and p.expiration_year <= 3030:
+                discard
             p.eyr = true
         elif thing=="hgt":
             p.hgt = true
@@ -90,6 +106,40 @@ proc part1(filename:string) =
 
     echo("valid count: ", valid_count)
 
+proc part2(filename:string) = 
+    var inf = open(filename)
+    defer: inf.close()
+
+    var valid_count = 0
+
+    var current : PD
+    clear_PD(current)
+
+    var line : string
+    while readLine(inf,line):
+        if line == "":
+    
+            echo("switching to new person",current)
+            if check_valid(current):
+                valid_count+=1
+
+            clear_PD(current)
+            person+=1
+
+        else:
+            let linedata = split(line)
+            for index,thing in linedata:
+                let thingdata = thing.split(":")
+                set_PD(thingdata,current)
+                echo(person, " -- ",index," ",thing, " ", thingdata)
+
+    echo("switching  to new person one last time",current)
+    if check_valid(current):
+        valid_count += 1
+    
+
+    echo("valid count: ", valid_count)
 
 # part1("test_input.txt")
-part1("input.txt")
+# part1("input.txt")
+part2("test_input.txt")
