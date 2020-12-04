d79ebc5f230650238d293d0a6ea163ea680a94bb
tony/gaming laptop upstairs <tonetheman@gmail.com>
2020-12-04 14:05:02 -0600

    saving work

--- day4/prog.nim
+++ day4/prog.nim
@@ -1,16 +1,48 @@
 
+import strutils
+var person : int = 1
+
+type PD = tuple
+    byr : bool
+    iyr : bool
+    eyr : bool
+    hgt : bool
+    hcl : bool
+    ecl : bool
+    pid : bool
+    cid : bool
+
+proc clear_PD(p : var PD) =
+    p.byr = false
+    p.iyr = false
+    p.eyr = false
+    p.hgt = false
+    p.hcl = false
+    p.ecl = false
+    p.pid = false
+    p.cid = false
 
 proc part1(filename:string) = 
     var inf = open(filename)
     defer: inf.close()
 
+    var current : PD
+    clear_PD(current)
 
     var line : string
     while readLine(inf,line):
         if line == "":
-            echo("blank")
+    
+            echo("switching to new person",current)
+            clear_PD(current)
+            person+=1
+
         else:
-            echo("not blank")
+            let linedata = split(line)
+            for index,thing in linedata:
+                let thingdata = thing.split(":")
+                set_PD(thingdata,current)
+                echo(person, " -- ",index," ",thing, " ", thingdata)
 
 
 
