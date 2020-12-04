0eb50c8ddbdc45ccb02ab6432841c21165642c73
agcc <tonetheman@gmail.com>
2020-12-04 16:10:48 -0600

    saving makefile

--- day4/prog.nim
+++ day4/prog.nim
@@ -22,9 +22,43 @@ proc clear_PD(p : var PD) =
     p.pid = false
     p.cid = false
 
+proc set_PD(thingdata: seq[string], p : var PD) =
+    # echo("set PD called ",p)
+    for thing in thingdata:
+        #  echo("\tcurrent thing : ", thing)
+        if thing=="byr":
+            p.byr = true
+        elif thing=="iyr":
+            p.iyr = true
+        elif thing=="eyr":
+            p.eyr = true
+        elif thing=="hgt":
+            p.hgt = true
+        elif thing == "hcl":
+            p.hcl = true
+        elif thing == "ecl":
+            p.ecl = true
+        elif thing=="pid":
+            p.pid = true
+        elif thing == "cid":
+            p.cid = true
+
+proc check_valid(p : PD ) : bool =
+    # everything true this si valid
+    if p.byr and p.iyr and p.eyr and p.hgt and p.hcl and p.ecl and p.pid and p.cid:
+        return true
+
+    # everything except cid is trur
+    if p.byr and p.iyr and p.eyr and p.hgt and p.hcl and p.ecl and p.pid:
+        return true
+
+    return false
+
 proc part1(filename:string) = 
     var inf = open(filename)
     defer: inf.close()
+
+    var valid_count = 0
 
     var current : PD
     clear_PD(current)
@@ -34,6 +68,9 @@ proc part1(filename:string) =
         if line == "":
     
             echo("switching to new person",current)
+            if check_valid(current):
+                valid_count+=1
+
             clear_PD(current)
             person+=1
 
@@ -44,8 +81,12 @@ proc part1(filename:string) =
                 set_PD(thingdata,current)
                 echo(person, " -- ",index," ",thing, " ", thingdata)
 
+    echo("switching  to new person one last time",current)
+    if check_valid(current):
+        valid_count += 1
+    
 
-
+    echo("valid count: ", valid_count)
 
 
 part1("test_input.txt")
