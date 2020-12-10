088829f9f1bca9cce6452829e7b64e93e3d1fbf1
tone <tonetheman@gmail.com>
2020-12-09 18:24:37 -0600

    done part1

--- day9/prog.nim
+++ day9/prog.nim
@@ -7,29 +7,45 @@ proc readfile(filename:string) : seq[int] =
     while readline(inf,line):
         result.add(parseInt(line))
 
+proc check(consider : seq[int], target : int ) : bool =
+    for index, val in consider:
+        for index2,val2 in consider:
+            if index!=index2:
+                if val+val2 == target:
+                    echo "\t",val," ",val2," ",val+val2, " ", target
+                    return true
+    return false
+
+
 proc part1() =
-    let filename = "test_input.txt"
+    let filename = "input.txt"
     var data = readfile(filename)
 
+    # echo data
 
-    let PREAMBLE = 5
+    let PREAMBLE = 25
+    var starting_index = 0
+    var current = PREAMBLE
 
-    let start_at = PREAMBLE-1
-    let beg = 0
-
-    var count = 0
     while true:
 
-        let check_these = data[beg..beg+PREAMBLE-1]
-        echo check_these
-        for i in countup(len(check_these)):
-            for j in countup(i,len(check_these)):
-                echo(i, " ", j, " ", check_these[i], " ", check_these[j])
-
-
-        count = count +  1
-        if count>10:
+        if current>len(data):
             break
+
+
+        let consider = data[starting_index..starting_index+PREAMBLE-1]
+        
+        echo "need to consider: ", consider, " ", data[current]
+
+        if check(consider,data[current]):
+            echo "FOUND A GOOD ONE"
+        else:
+            echo "FOUND A BAD ONE"
+            break
+
+        starting_index += 1
+        current += 1
+        
 
 
 part1()
