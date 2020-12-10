a76b87a16dba3486f9cbb4594bcdfa09142f9fbb
tone <tonetheman@gmail.com>
2020-12-10 16:08:00 -0600

    day 2 i will not finish it

--- day10/prog.nim
+++ day10/prog.nim
@@ -18,26 +18,75 @@ const filename = "input.txt"
 var data = readfile(filename)
 data.sort()
 
-var charging_outlet = 0
-var current = 0
-var counts  = initTable[int,int]()
+proc checkit(data : seq[int]) : bool = 
+  var charging_outlet = 0
+  var current = 0
+  var counts  = initTable[int,int]()
 
-while true:
+  while true:
 
-    if current == len(data):
-        break
+      if current == len(data):
+          break
 
-    echo "current: ", current, " charging outlet: ", charging_outlet, "  data[current] = ", data[current]
-    let diff = data[current]-charging_outlet
-    if counts.hasKey(diff):
-        counts[diff] += 1
+      # echo "current: ", current, " charging outlet: ", charging_outlet, "  data[current] = ", data[current]
+      let diff = data[current]-charging_outlet
+      if diff < 1:
+        return false
+      if diff > 3:
+        return false
+      if counts.hasKey(diff):
+          counts[diff] += 1
+      else:
+          counts[diff] = 1
+      
+      current += 1
+      charging_outlet += diff
+
+  # add the last diff of 3
+  counts[3] += 1
+
+  # echo counts, counts[3] * counts[1]    
+  return true
+
+
+
+proc part1() =
+  # const filename = "test_input.txt"
+  # const filename = "test_input2.txt"
+  # const filename = "input.txt"
+  var data = readfile(filename)
+  data.sort()
+  echo checkit(data)
+
+proc part2() =
+  # const filename = "test_input.txt"
+  # const filename = "test_input2.txt"
+  # const filename = "input.txt"
+  var data = readfile(filename)
+  data.sort()
+
+  var resit = true
+  var good = 0
+  var bad = 0
+  var count = 0
+
+  while resit:
+
+    if checkit(data):
+      good += 1
     else:
-        counts[diff] = 1
-    
-    current += 1
-    charging_outlet += diff
+      bad += 1
 
-# add the last diff of 3
-counts[3] += 1
+    resit = data.nextPermutation()
+    if resit == false:
+      break
+  
+    count += 1
+    if count mod 100000 == 0:
+      echo "count: ",count, "bad: ",bad, " good: ",good
 
-echo counts, counts[3] * counts[1]    
+  echo "good: ", good
+  echo "Bad: ", bad
+
+part2()
+
---
+++ day10/readme.md
@@ -0,0 +1,4 @@
+
+# part2
+
+something to do with the difference for a valid one always has to be positve of 1 2 or 3
